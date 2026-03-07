e7855881930e52db1ab51e53c68545cd16693e77
ALEKSANDROOOOO <riabkovsasha2707@gmail.com>
2026-03-07 03:58:32 +0300

    minor changes

---
+++ second_semester/LR2. NumPy/main.py
@@ -0,0 +1,28 @@
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+def create_vector(n: int) -> np.ndarray:
+    """
+    Создает матрицу размерности 1xn (вектор)
+    
+    Returns:
+        numpy.ndarray: массив чисел от 0 до n-1 включительно
+        
+    Example:
+        >>>
+        
+    """
+    
+    return np.ndarray(10)
+
+a = create_vector(10)
+print(a)
+
+
+# b = np.random.rand(5, 5)
+# print(b, type(b))
+
